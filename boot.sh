1638ca931de83a446d820798856f632c93fa67e0
aldum <aldum@artixlinux.org>
2025-07-05 12:29:56 +0200

    refactor: install git if needed

--- boot.sh
+++ boot.sh
@@ -13,7 +13,7 @@ ascii_art='
 
 echo -e "\n$ascii_art\n"
 
-pacman -Q git &>/dev/null || sudo pacman -Sy --noconfirm --needed git
+sudo pacman -Sy --noconfirm --needed git
 
 echo -e "\nCloning Omarchy..."
 rm -rf ~/.local/share/omarchy/
@@ -29,3 +29,4 @@ fi
 
 echo -e "\nInstallation starting..."
 source ~/.local/share/omarchy/install.sh
+
